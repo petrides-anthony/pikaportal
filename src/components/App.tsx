140c52d0fd33e54bf2a4edb0995304b6bd46e439
Anthony Petrides <anthony.petrides@thetrainline.com>
2021-09-01 15:50:44 +0100

    add pikaponents and replace awesome-ts-loader with ts-loader

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,22 @@
 import * as React from "react";
+import { Button, Header } from 'pikaponents';
 
 export interface HelloWorldProps {
   userName: string;
   lang: string;
 }
 export const App = (props: HelloWorldProps) => (
-  <h1>
-    Hi {props.userName} from React! Welcome to {props.lang}!
-  </h1>
+  <div>
+    <Header 
+      headerIconLink="https://www.google.co.uk"
+      headerText="I'm from Pikaponents"
+      navLinkText1="Home"
+      navLinkText2="About"
+      >
+      </Header>
+    <Button label="Testing"></Button>
+    <h1>
+      Hi {props.userName} from React! Welcome to {props.lang}!
+    </h1>
+  </div>
 );
